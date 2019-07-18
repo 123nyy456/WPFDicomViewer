ff0da0176e390254d53505002ad585a36ee84c2a
yy n <123nyy456@gmail.com>
2019-07-18 23:45:57 +0800

    DicomViewer First Commit

---
+++ DicomViewer/DcmFileManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dicom;
+
+namespace DicomViewer
+{
+    class DcmFileManager
+    {
+        private int win_width;
+        private int win_location;
+        private int img_with;
+        private int img_height;
+        public DcmFileManager()
+        {
+
+        }
+        public int WinWidth
+        {
+            get
+            {
+                return win_width;
+            }
+            
+            set
+            {
+                win_width = value;
+            }
+        }
+        public void OpenDcmFile(string file_path)
+        {
+
+        }
+    }
+}
