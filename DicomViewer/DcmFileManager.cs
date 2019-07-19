4fd5ff81aa0bd11552931097c16c1e7db0cc6053
NYY-UIH <123nyy456@gmail.com>
2019-07-19 13:26:08 +0800

    Dicom Image Read Finish

--- DicomViewer/DcmFileManager.cs
+++ DicomViewer/DcmFileManager.cs
@@ -1,39 +1,95 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 using Dicom;
+using Dicom.Imaging;
 
 namespace DicomViewer
 {
     class DcmFileManager
     {
-        private int win_width;
-        private int win_location;
-        private int img_with;
-        private int img_height;
+        private double _win_width;
+        private double _win_center;
+        private int _img_with;
+        private int _img_height;
+        private string _file_path;
+        private DicomFile _dcm_file;
+        private DicomImage _dcm_image;
         public DcmFileManager()
         {
-
+            ImageManager.SetImplementation(WPFImageManager.Instance);
         }
-        public int WinWidth
+        public double WinWidth
         {
             get
             {
-                return win_width;
+                return _win_width;
             }
-            
             set
             {
-                win_width = value;
+                _win_width = value;
             }
         }
 
+        public double WinCenter
+        {
+            get
+            {
+                return _win_center;
+            }
+            set
+            {
+                _win_center = value;
+            }
+        }
+
+        public int ImgWidth
+        {
+            get
+            {
+                return _img_with;
+            }
+        }
+
+        public int ImgHeight
+        {
+            get
+            {
+                return _img_height;
+            }
+        }
+
+        public string DcmFilePath
+        {
+            get { return _file_path; }
+        }
 
         public void OpenDcmFile(string file_path)
         {
+            _file_path = file_path;
+            _dcm_file = DicomFile.Open(_file_path);
+            _dcm_image = new DicomImage(_dcm_file.Dataset, 0);
+            _img_with = _dcm_image.Width;
+            _img_height = _dcm_image.Height;
+            _win_width = _dcm_image.WindowWidth;
+            _win_center = _dcm_image.WindowCenter;
+        }
 
+        public WriteableBitmap RendImage(double offsetX, double offsetY)
+        { 
+            _dcm_image.WindowWidth = _win_width + offsetX;
+            _dcm_image.WindowCenter = _win_center + offsetY;
+            if (_dcm_image == null)
+            {
+                return null;
+            }
+
+            WriteableBitmap renderedImage = _dcm_image.RenderImage().As<WriteableBitmap>();
+            return renderedImage;
         }
     }
 }
--- DicomViewer/MainWindow.xaml.cs
+++ DicomViewer/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace DicomViewer
 {
@@ -20,14 +21,55 @@ namespace DicomViewer
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DcmFileManager _dcm_file_mgr;
+        private Point _ptDown;
         public MainWindow()
         {
             InitializeComponent();
+            _dcm_file_mgr = new DcmFileManager();
         }
 
-        private void btn_OpenImage_Click(object sender, RoutedEventArgs e)
+        private void Btn_OpenImage_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("!!!");
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false;
+            dialog.Title = "请选择Dicom文件";
+            dialog.Filter = "所有文件(*.*)|*.*";
+            dialog.InitialDirectory = "D:\\";
+            string dcmFileName = "";
+            if (dialog.ShowDialog() == true)
+            {
+                dcmFileName = dialog.FileName;
+            }
+            _dcm_file_mgr.OpenDcmFile(dcmFileName);
+            Update_Image();
+        }
+
+        private void Img_LeftButtonDown(object sender, MouseEventArgs e)
+        {
+            _ptDown = e.GetPosition(img);
+        }
+
+        private void Img_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                Point ptCurrent = e.GetPosition(img);
+                Update_Image(ptCurrent.X - _ptDown.X, ptCurrent.Y - _ptDown.Y);
+            }
+        }
+
+        private void Img_LeftButtonUp(object sender, MouseEventArgs e)
+        {
+            Point ptCurrent = e.GetPosition(img);
+            _dcm_file_mgr.WinWidth += ptCurrent.X - _ptDown.X;
+            _dcm_file_mgr.WinCenter += ptCurrent.Y - _ptDown.Y;
+            Update_Image();
+        }
+
+        private void Update_Image(double offsetX = 0, double offsetY = 0)
+        {
+             img.Source = _dcm_file_mgr.RendImage(offsetX, offsetY);
         }
     }
 }
